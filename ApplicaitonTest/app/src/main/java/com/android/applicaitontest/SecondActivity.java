5f7333068b191760ff3e985b8ba619ccbc8bc372
wuxiaohui94 <wuxiaohui94@126.com>
2022-11-08 02:07:55 +0800

    all

---
+++ ApplicaitonTest/app/src/main/java/com/android/applicaitontest/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.android.applicaitontest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Person person = (Person) getIntent().getSerializableExtra("person_data");
+                Log.d("SecondActivity",person.toString());
+            }
+        });
+    }
+}
